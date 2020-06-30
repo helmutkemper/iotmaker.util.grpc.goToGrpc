fb3d33b8cca16ce31c4dce2f5e1457a019aa0e43
Helmut Kemper <helmut.kemper@gmail.com>
2020-06-30 18:16:17 -0300

    initial commit

--- main/kemper_client/main.go
+++ main/kemper_client/main.go
@@ -16,8 +16,9 @@ const (
 )
 
 func main() {
-	// Set up a connection to the server.
-	var r *pb.ContainerInspectReply
+	var ci *pb.ContainerInspectReply
+	var ni *pb.NetworkInspectReply
+
 	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
@@ -26,9 +27,15 @@ func main() {
 	c := pb.NewDockerServerClient(conn)
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
-	r, err = c.ContainerInspect(ctx, &pb.ContainerInspectRequest{ID: "0b85a0b07ad6ef643df3c6c4f91a728aa3c171cbcf067f961f13ed7e3702fb8b"})
+	ci, err = c.ContainerInspect(ctx, &pb.ContainerInspectRequest{ID: "0b85a0b07ad6ef643df3c6c4f91a728aa3c171cbcf067f961f13ed7e3702fb8b"})
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
-	fmt.Printf("ID: %s\nError: %+v\n", r.GetID(), r.GetError())
+	fmt.Printf("ID: %s\n", ci.GetID())
+
+	ni, err = c.NetworkInspect(ctx, &pb.NetworkInspectRequest{ID: "0fdb93c956338309125d1ff94e440448d1b70e2de29626260451bdda4b8d8226"})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	fmt.Printf("ID: %s\n", ni.GetID())
 }
